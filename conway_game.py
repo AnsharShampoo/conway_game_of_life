8384ae4eef92eba60cbf11682c4b40a9f49d835f
AnsharShampoo <anshar@ciencias.unam.mx>
2024-04-06 03:13:58 -0600

    v0.1 first functional iteration

--- conway_game.py
+++ conway_game.py
@@ -1,6 +1,8 @@
 from conway_colors import conway_colors
 import pygame
 import sys
+clock = pygame.time.Clock()
+
 def print_grid(screen,size):
         #set grid
     for x in range(0,size[0],10):
@@ -13,26 +15,98 @@ def get_squares(screen,squares,size):
         for y in range(size[1]):
             pygame.draw.rect(screen,conway_colors.BLACK,(x*10,y*10,10,10)) if squares[x][y]==0 else pygame.draw.rect(screen,conway_colors.WHITE,(x*10,y*10,10,10))
 
-def event_manager(screen, squares):
+def event_manager(screen, squares, size):
     for event in pygame.event.get():
-        if event.type == pygame.K_q or event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x,mouse_y = pygame.mouse.get_pos()
             pygame.draw.rect(screen,conway_colors.WHITE,(mouse_x-(mouse_x%10),mouse_y-(mouse_y%10),10,10))
             print(f"{int((mouse_x-(mouse_x%10))/10)}:::{int((mouse_y-(mouse_y%10))/10)}")
             squares[int((mouse_x-(mouse_x%10))/10)][int((mouse_y-(mouse_y%10))/10)] = 1 if squares[int((mouse_x-(mouse_x%10))/10)][int((mouse_y-(mouse_y%10))/10)]==0 else 0
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                start_simulation(screen,squares,size)
 
-def main():
-    pygame.init()
-    size = (100,200)
-    screen = pygame.display.set_mode(size)
-    squares = [[0 for i in range(size[1])] for j in range(size[0])]
+def conway_grid(squares, new_squares,size):
+    for x in range(size[0]):
+            for y in range(size[1]):
+                alive_neighbours = check_neighbours(x,y,squares,size)
+                if(squares[x][y]==1 and (alive_neighbours==2 or alive_neighbours==3)):
+                    new_squares[x][y]=1
+                elif(squares[x][y]==0 and alive_neighbours==3):
+                    new_squares[x][y]=1
+    return new_squares
+
+def start_simulation(screen,squares,size):
+    #RULES
+    #Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+    #Any live cell with two or three live neighbors lives on to the next generation.
+    #Any live cell with more than three live neighbors dies, as if by overpopulation.
+    #Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+    running = True
+    new_squares = [[0 for i in range(size[1])] for j in range(size[0])]
+    while running:
+        conway_grid(squares,new_squares,size)
+        for x in range(size[0]):
+            for y in range(size[1]):
+                squares[x][y] = new_squares[x][y]
+        get_squares(screen,new_squares,size)
+        for x in range(size[0]):
+            for y in range(size[1]):
+                new_squares[x][y] = 0
+        print_grid(screen,size)
+        simulation_event_manager(screen, new_squares, size)
+        pygame.display.flip()
+        clock.tick(60)
+
+def check_neighbours(x,y,squares,size):
+    alive_cells = 0
+    if(x>0 and y>0):
+        alive_cells+=squares[x-1][y-1]
+        alive_cells+=squares[x][y-1]
+        alive_cells+=squares[x-1][y]
+    elif(x>0):
+        alive_cells+=squares[x-1][y]
+    elif(y>0):
+        alive_cells+=squares[x][y-1]
+    if(x<size[0]-1 and y<size[1]-1):
+        alive_cells+=squares[x+1][y+1]
+        alive_cells+=squares[x][y+1]
+        alive_cells+=squares[x+1][y]
+    elif(x<size[0]-1):
+        alive_cells+=squares[x+1][y]
+    elif(y<size[1]-1):
+        alive_cells+=squares[x][y+1]
+    if(x>0 and y<size[1]-1):
+        alive_cells+=squares[x-1][y+1]
+    if(y>0 and x<size[0]-1):
+        alive_cells+=squares[x+1][y-1]
+    return alive_cells
+
+def simulation_event_manager(screen, squares, size):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                main_event(screen,squares,size)
+
+def main_event(screen,squares,size):
     while True:
         get_squares(screen,squares,size)
         print_grid(screen,size)
-        event_manager(screen, squares)
+        event_manager(screen, squares, size)
         pygame.display.flip()
+        clock.tick(60)
+
+def main():
+    pygame.init()
+    clock = pygame.time.Clock()
+    size = (500,500)
+    screen = pygame.display.set_mode(size)
+    squares = [[0 for i in range(size[1])] for j in range(size[0])]
+    main_event(screen,squares,size)
 
 if __name__ == '__main__':
     main()
